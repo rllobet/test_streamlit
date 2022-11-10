c94426b810dce0476e1cfdb771cca0db79db0385
Rafa <rllobet@garbi.local>
2022-11-10 09:42:51 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def cel2far(c):
+    return c*9/5+32
+
+
+st.title('Conversor grados')
+tc = st.slider('Temp Cel.', min_value=-50, max_value=50, value=0)
+
+if st.button('Convertir'):
+    tf = cel2far(tc)
+    st.text_input('Fahrenheit: ', value=tf)
