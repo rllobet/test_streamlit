e8a06898fc36ae710a9fc9003e090ce803ad0ed9
Rafa <rllobet@garbi.local>
2022-11-12 18:42:10 +0100

    Trying more features

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import streamlit as st
 
+header = st.beta_container()
+temp_conv = st.beta_container()
+tiro = st.beta_container()
+
+
 def cel2far(c):
     return c*9/5+32
 
 
-st.title('Conversor grados')
-tc = st.slider('Temperatura Celsius', min_value=-50, max_value=50, value=0)
+with header:
+    st.title('Informática y Programación')
+    st.header('Grado en Ingeniería Física')
+    st.text('Universitat Politècnica de Valéncia')
+    
+with temp_conv:
+    st.title('Conversor grados')
+    tc = st.slider('Temperatura Celsius', min_value=-50, max_value=50, value=0)
 
-if st.button('Convertir'):
-    tf = cel2far(tc)
-    st.text_input('Fahrenheit: ', value=tf)
+    if st.button('Convertir'):
+        tf = cel2far(tc)
+        st.text_input('Fahrenheit: ', value=tf)
